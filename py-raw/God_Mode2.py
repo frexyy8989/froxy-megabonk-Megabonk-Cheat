1e3e16c8a96d11e0d8db454eb6e3aa4fea72b450
frexyy8989 <frexy8989@gmail.com>
2026-03-18 17:30:00 +0300

    Froxy-Megabonk V1.1.1

--- py-raw/God_Mode2.py
+++ py-raw/God_Mode2.py
@@ -1,32 +1,23 @@
 import pymem
 import pymem.process
 
-# 1. Oyuna bağlan (Örnek: megabonk.exe ise onu yaz)
 try:
-    pm = pymem.Pymem("Megabonk.exe") # Burayı oyunun .exe adıyla değiştir!
+    pm = pymem.Pymem("Megabonk.exe")
     print("Oyuna başarıyla bağlanıldı.")
 except Exception as e:
     print(f"Hata: Oyun bulunamadı veya yönetici olarak çalıştırılmadı. {e}")
     exit()
 
-# 2. GameAssembly.dll modülünün başlangıç adresini bul
-module = pymem.process.module_from_name(pm.process_handle, "GameAssembly.dll").lpBaseOfDll
+module = pymem.process.module_from_name(pm.process_handle, "UnityPlayer.dll").lpBaseOfDll
 
-# 3. Pointer Okuma Fonksiyonu (Dinamik adresi hesaplar)
 def get_pointer_address(base, offsets):
-    # Oyun muhtemelen 64-bit (1A9... ile başladığı için), read_longlong kullanıyoruz
     addr = pm.read_longlong(base)
     for offset in offsets[:-1]:
         addr = pm.read_longlong(addr + offset)
     return addr + offsets[-1]
 
-# --- SENİN GÖRÜNTÜNDEKİ DEĞERLER ---
-# Base Static Address: GameAssembly.dll + 02F840B8
-base_static = module + 0x02F83018
-
-# Offsetler: En alttan en üste doğru sıralı liste
-# Görüntündeki kutucukları aşağıdan yukarıya doğru yazdık
-my_offsets = [0x40, 0xB8, 0x8, 0x78, 0x40, 0x160, 0x4BC]
+base_static = module + 0x01C6A7C8
+my_offsets = [0x4C8, 0x10, 0x0, 0x10, 0x18, 0x0, 0x13C]
 
 try:
     # Dinamik adresi bul
@@ -39,9 +30,6 @@ try:
             pm.write_int(dynamic_address,0)
     except Exception as e:
         print(f"God mode açılırken bir sorun oluştu: {e}")
-    # EĞER DEĞERİ DEĞİŞTİRMEK İSTERSEN:
-    # pm.write_int(dynamic_address, 1337)
-    # print("Değer başarıyla değiştirildi!")
 
 except Exception as e:
     print(f"Adres hesaplanırken hata oluştu (Oyun kapalı veya harita yüklenmemiş olabilir): {e}")
